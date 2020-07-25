be332c02e8adcd6e7995b907ea15c76e2b537090
Jash <scyther67@gmail.com>
2020-07-25 14:43:17 +0530

    auth routes added

---
+++ backend/api/policies/userAuth.js
@@ -0,0 +1,24 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const jwtKey  = require("../../config/loadConfig").JWT;
+
+const  opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = jwtKey;
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        // console.log(jwt_payload);
+        User.ById(jwt_payload.id)
+            .then(user => {
+                if(user) {
+                    return done(null, user);
+                }
+
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
+    }));
+};
