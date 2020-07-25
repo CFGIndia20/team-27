a53687510f1902dcf1d26eff42f6cfa0c036f8bb
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-07-25 21:58:29 +0530

    Resolved merge conflicts

--- backend/api/models/admin.js
+++ backend/api/models/admin.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Admin = new mongoose.Schema({
     // addedJobs: [{
     //     active: {type: Boolean, default: false},
-    //     job: {type: mongoose.SchemaTypes.ObjectId, ref: 'Job'}
+    //     job: {type: mongoose.Schema.Types.ObjectId, ref: 'Job'}
     // }],
     slotsCreated: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Slot'}],
 });
