b988c8b076cb46a5f563554afb115f3870fa8260
Jash <scyther67@gmail.com>
2020-07-25 17:57:23 +0530

    auth reinstated

--- backend/api/policies/teacherAuth.js
+++ backend/api/policies/teacherAuth.js
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-const JwtStrategy = require('passport-jwt').Strategy,
-ExtractJwt = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-=======
 const {verify} = require('../utils/jwt');
 const logger = require('../../config/winston');
 const {AuthError} = require('../responses');
->>>>>>> master
 const User = require('../models/user');
 
 
-<<<<<<< HEAD
-module.exports = passport => {
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        // console.log(jwt_payload);
-        User.findOne({ _id : jwt_payload.id , access : "teacher"})
-            .then(user => {
-                if(user) {
-                    return done(null, user);
-                }
-
-                return done(null, false);
-            })
-            .catch(err => console.log(err));
-    }));
-};
-=======
 /**
  * @desc Standard Express middleware for checking JSON token, and sending refresh token if expired
  * */
@@ -47,4 +25,3 @@ module.exports = async (req, res, next) => {
     return res.status(401).json(AuthError);
   }
 };
->>>>>>> master
--- backend/api/policies/userAuth.js
+++ backend/api/policies/userAuth.js
@@ -4,16 +4,6 @@ const {AuthError} = require('../responses');
 const User = require('../models/user');
 
 
-<<<<<<< HEAD
-module.exports = passport => {
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        // console.log(jwt_payload);
-        User.findOneById(jwt_payload.id)
-            .then(user => {
-                if(user && user.access == 'student') {
-                    return done(null, user);
-                }
-=======
 /**
  * @desc Standard Express middleware for checking JSON token, and sending refresh token if expired
  * */
@@ -25,7 +15,6 @@ module.exports = async (req, res, next) => {
     if (token.success != true || token.type != 'access_token') return res.status(401).json(AuthError);
     const user = await User.findOne({_id: token.id, access: 'user'});
     if (user == null) return res.status(401).json(AuthError);
->>>>>>> master
 
     req.body.userId = token.id;
     req.body.access = user.access;
