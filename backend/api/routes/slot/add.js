876bce547003fee40c5cad53d48fafb7387937b6
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2020-07-25 15:47:24 +0530

    Added add a slot route

---
+++ backend/api/routes/slot/add.js
@@ -0,0 +1,16 @@
+const {addSlot} = require('../../dbFunctions/slot');
+const logger = require('../../../config/winston');
+const {ServerError, Success} = require('../../responses');
+
+module.exports = async (req, res) => {
+    try {
+
+        const {userId, startDate, endTime, startTime} = req.body;
+        const added = await addSlot(userId, startDate, startTime, endTime);
+        if (added == null) return res.json({...ServerError, message: "Error creating the slot"});
+        return res.json({...Success, id: added._id});
+    } catch {
+        logger.error({err:error, message: "An error occured"});
+        return res.json(ServerError);
+    }
+}
